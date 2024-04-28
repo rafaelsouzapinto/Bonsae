313c376838576eb09e37dfc90faccefd7795408b
Austin.Dev <130511498+Austinmff@users.noreply.github.com>
2024-04-27 21:43:44 -0300

    Automatização do carrossel

---
+++ script.js
@@ -0,0 +1,15 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); 
