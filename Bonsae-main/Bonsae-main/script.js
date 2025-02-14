a8d612c0be6f2edce6c8a07e24483b853cc88877
Pedro Gois <145384092+PedroGoiis@users.noreply.github.com>
2025-02-14 19:40:10 -0300

    Add files via upload

---
+++ Bonsae-main/Bonsae-main/script.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : "function";
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); 
+
